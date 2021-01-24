be85a9d75d3b6e8f8815ca8b97630a37efa5e102
Igor Moura <imphilippini@gmail.com>
2021-01-24 13:55:57 -0300

    feat: add method to write csv file

--- exporter.py
+++ exporter.py
@@ -1,4 +1,7 @@
+import re
 import requests
+
+import pandas as pd
 from bs4 import BeautifulSoup
 
 
@@ -10,11 +13,14 @@ class Parser:
         self.user = user
         self.movies_parsed = 0
 
+        # List containing tuples of (title, director, year, rating)
+        self.movies_list = []
+
         self.parse(user)
 
     def parse(self, user):
         self.page = 1
-        last_page = self.get_last_page(user)
+        last_page = self.__get_last_page(user)
 
         while self.page <= last_page:
             url = (
@@ -32,6 +38,7 @@ class Parser:
                 raise Exception
 
             for movie in soup.find_all("li", {"class": "movie_list_item"}):
+
                 nota = (
                     movie.findChild("span", {"class": "star-rating"})
                     .get("title")
@@ -39,16 +46,24 @@ class Parser:
                 )
 
                 movie_page = movie.findChild("a", {"class": "tip-movie"}).get("href")
-                title, director, year = self.parse_movie(
+                title, director, year = self.__parse_movie(
                     "https://filmow.com" + movie_page
                 )
-                print(title, director, year, nota)
+                self.movies_list.append((title, director, year, nota))
                 self.movies_parsed += 1
-                break
             self.page += 1
             break
 
-    def parse_movie(self, url):
+    def get_df(self):
+        cols = ["Title", "Directors", "Year", "Rating"]
+        df = pd.DataFrame(self.movies_list, columns=cols)
+        return df
+
+    def write_csv(self):
+        df = self.get_df()
+        df.to_csv(self.user + ".csv", index=False)
+
+    def __parse_movie(self, url):
 
         title = None
         director = None
@@ -85,7 +100,7 @@ class Parser:
 
         return (title, director, year)
 
-    def get_last_page(self, user):
+    def __get_last_page(self, user):
         url = "https://filmow.com/usuario/" + user + "/filmes/ja-vi/"
 
         source_code = requests.get(url).text
@@ -102,4 +117,6 @@ class Parser:
             return 1
 
 
-Parser("imp2")
+parse = Parser("imp2")
+
+parse.write_csv()
